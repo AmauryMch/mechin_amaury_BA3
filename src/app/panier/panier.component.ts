844df97b3d90b339faba9d82c5a05656f7b02609
Amaury <amaurymechin37@gmail.com>
2024-03-05 15:10:05 +0100

    Navbar

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EpanierServiceService } from '../services/epanier-service.service';
+import { IArticlePanier } from '../interfaces/articlePanier.interface';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-panier',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './panier.component.html',
   styleUrl: './panier.component.css'
 })
-export class PanierComponent {
+export class PanierComponent implements OnInit {
+  articlesPanier!: IArticlePanier[];
+
+  constructor(private epanierservice: EpanierServiceService) { }
+
+  ngOnInit(): void {
+    this.articlesPanier = this.epanierservice.getPanier();
+  }
 
 }
