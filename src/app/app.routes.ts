ad0d0a699a96d87b21eab913d7f3e5c44471ac1d
Amaury <amaurymechin37@gmail.com>
2024-03-05 14:17:30 +0100

    Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { PanierComponent } from './panier/panier.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "accueil", component: AccueilComponent },
+    { path: "panier", component: PanierComponent },
+    { path: "", component: AccueilComponent }
+];
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NgIf, RouterLink, RouterOutlet],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
